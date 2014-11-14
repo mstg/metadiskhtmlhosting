b5bcabd52a31956c8ac8d81949e63bb04ec1b60e
switchpwn <mustiigezen@gmail.com>
2014-11-14 04:10:15 +0100

    Stylesheet serving now works

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	$url = 'http://node1.metadisk.org/api/download/facb981c0df9b96b681849bcc8955c4e228625ef7f01e4358dce877d3476c404?key=a286790cfb84b13e4c7fff9c01043cbd94d1bc016c513166935ecf4b6102458c&token=UqaWt2Pckt0sAHhV';
+	$url = 'http://node1.metadisk.org/api/download/c3b347c10a932fc4f229c9d3da0908402ce9434ab1b9a5be7e1e8e093969ac18?key=a7fc7a7849c55ff8882917060c9fd37a4aaecee5df51c1525a274c7039f968ad&token=UqaWt2Pckt0sAHhV';
 	$filelist = file_get_contents($url);
 
 	$files = explode("\n", $filelist);
@@ -13,15 +13,38 @@
 		$fileandurl = explode(":", $file);
 
 		if ($fileandurl[0] == $page) {
-			echo file_get_contents("http:" . $fileandurl[2]);
-			echo "http:" . $fileandurl[2] . "<br \>" . "Showing file: " . $fileandurl[0];
+			$html = file_get_contents("http:" . $fileandurl[2]);
+
+			$html = $html . "<br />" . "http:" . $fileandurl[2] . "<br \>" . "Showing file: " . $fileandurl[0];
 		}
 	}
+
+	foreach ($files as $file) {
+		$fileandurl = explode(":", $file);
+
+		$page = str_replace(".html", ".css", $page);
+
+		if ($fileandurl[0] == $page) {
+			$css = "<style type='text/css'>" . file_get_contents("http:" . $fileandurl[2]) . "</style>";
+			$html = str_replace('$css', $css, $html);
+		}
+	}
+
+	echo $html;
 
 	echo "<br/><br/>File list:<br />";
 	foreach ($files as $file) {
 		$fileandurl = explode(":", $file);
 
-		echo "<a href='/?page=" . $fileandurl[0] . "'>" . $fileandurl[0] . "</a><br />";
+		if (strpos($fileandurl[0], ".css") === false)
+			echo "<a href='/?page=" . $fileandurl[0] . "'>" . $fileandurl[0] . "</a><br />";
+	}
+
+	echo "<br/>Stylesheet list:<br />";
+	foreach ($files as $file) {
+		$fileandurl = explode(":", $file);
+
+		if (strpos($fileandurl[0], ".css") !== false)
+			echo $fileandurl[0] . " - " . "http:" . $fileandurl[2];
 	}
 ?>
